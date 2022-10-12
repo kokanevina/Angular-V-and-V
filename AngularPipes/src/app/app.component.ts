a6a01c0aa9e34919e45bbdf7b4db34463670cc70
kokanevina <kokane.vina@gmail.com>
2022-10-12 12:35:04 +0530

    pipes

--- AngularPipes/src/app/app.component.ts
+++ AngularPipes/src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
  
  empArray:Employee[]=[
-  new Employee(11,"kuMAR",78000.563783,new Date(2000, 6, 2)),
-  new Employee(10,"Mohan",88000.6,new Date(2005, 5, 1)),
+  new Employee(11,"kuMAR goshwami",78000.563783,new Date(2000, 6, 2)),
+  new Employee(10,"Mohan Bhagwat",88000.6,new Date(2005, 5, 1)),
   new Employee(15,"rani poojari",58000.44444,new Date(2002, 8, 22)),
-  new Employee(12,"komal",68000.45345,new Date(2010, 12, 12,13,20,0)),
-  new Employee(16,"kishori",65000.55544,new Date(2010, 12, 11,10,30,0)),
+  new Employee(12,"komal Dusane",68000.45345,new Date(2010, 12, 12,13,20,0)),
+  new Employee(16,"kishori shahane",65000.55544,new Date(2010, 12, 11,10,30,0)),
  ];
 
 }
